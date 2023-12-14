97b01a72a15362cddce4784439f947fe87a50a81
Daniel oliveira <danield2game@gmail.com>
2023-12-14 12:04:15 -0300

    Versão Consertada

--- Atividade de comentario/src/Placa.java
+++ Atividade de comentario/src/Placa.java
@@ -1,15 +1,54 @@
+import java.util.Scanner;
+
 public class Placa {
+    private String letra;
+    private int numero;
 
-//Placa pois se escreve algo nela
-    //public = palavra Reservada
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in)
-        //Aqui  usuario escrevera algo que sera escrito na pçaca
-        System.out.print("Por favor, escreva algo Para ser colocado na placa: ");
-
-        //Aqui irar ler oque o usuario dogitou e salvar em uma variavel
-        String entradaUsuario = scanner.nextLine();
-        //Imprimi oque o usuario escreveu
-        System.out.println("Você escreveu: " + entradaUsuario);
-
+    // Construtor da classe Placa
+    public Placa(String letra, int numero) {
+        this.letra = letra;
+        this.numero = numero;
     }
+
+    // Getter para letra
+    public String getLetra() {
+        return letra;
+    }
+
+    // Setter para letra
+    public void setLetra(String novaLetra) {
+        this.letra = novaLetra;
+    }
+
+    // Getter para numero
+    public int getNumero() {
+        return numero;
+    }
+
+    // Setter para numero
+    public void setNumero(int novoNumero) {
+        this.numero = novoNumero;
+    }
+
+    // Método para obter a representação da placa
+    public String obterPlaca() {
+        return letra + "-" + numero;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar ao usuário a entrada para a placa
+        System.out.print("Por favor, escreva a letra da placa: ");
+        String letraPlaca = scanner.nextLine();
+
+        System.out.print("Agora, escreva o número da placa: ");
+        int numeroPlaca = scanner.nextInt();
+
+        // Criar uma instância da classe Placa
+        Placa placa = new Placa(letraPlaca, numeroPlaca);
+
+        // Imprimir a representação da placa
+        System.out.println("Você escreveu: " + placa.obterPlaca());
+    }
+}
